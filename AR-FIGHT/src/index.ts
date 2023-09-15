63a90661e3a309109db5c7ecd08d837c0ee24846
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-09-15 22:17:51 +0530

    added ar fight folder

---
+++ AR-FIGHT/src/index.ts
@@ -0,0 +1,381 @@
+// @ts-nocheck
+/// Zappar for ThreeJS Examples
+/// Instant Tracking 3D Model
+
+// In this example we track a 3D model using instant world tracking
+
+import * as THREE from "three";
+import * as ZapparThree from "@zappar/zappar-threejs";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GUI } from "dat.gui";
+
+let isPokeballRotating = false;
+
+// const model = new URL("../assets/pikachu2.glb", import.meta.url).href;
+// const pokemon1 = new URL("../assets/peakpx-2.jpg", import.meta.url).href;
+// const pokemon2 = new URL("../assets/chari.png", import.meta.url).href;
+
+// const pokeball = new URL(
+//   "../assets/pokemon_basic_pokeball.glb",
+//   import.meta.url
+// ).href;
+
+// const frame = new URL("../assets/picture_frame.glb", import.meta.url).href;
+
+import "./index.css";
+import { formToJSON } from "axios";
+
+if (ZapparThree.browserIncompatible()) {
+  // The browserIncompatibleUI() function shows a full-page dialog that informs the user
+  // they're using an unsupported browser, and provides a button to 'copy' the current page
+  // URL so they can 'paste' it into the address bar of a compatible alternative.
+  ZapparThree.browserIncompatibleUI();
+
+  // If the browser is not compatible, we can avoid setting up the rest of the page
+  // so we throw an exception here.
+  throw new Error("Unsupported browser");
+}
+
+const manager = new ZapparThree.LoadingManager();
+
+// ==================== Selectings dom elemets ====================
+const canvas = document.querySelector("canvas.webgl");
+const button_six = document.querySelector(".six");
+const forwardButton = document.querySelector(".forward");
+const jumpButton = document.querySelector(".jump");
+const buttonOne = document.getElementById("button1");
+const buttonTwo = document.getElementById("button2");
+
+// index.ts
+document.addEventListener("DOMContentLoaded", () => {
+  const countdownElement = document.getElementById("countdown");
+  const tapToPlaceElement = document.getElementById("tap-to-place");
+
+  // Function to start the countdown animation
+  function startCountdown() {
+    countdownElement.innerText = "3";
+    countdownElement.classList.add("animate-countdown");
+    setTimeout(() => {
+      countdownElement.innerText = "2";
+      setTimeout(() => {
+        countdownElement.innerText = "1";
+        setTimeout(() => {
+          // Start your game logic or remove the countdown element
+          countdownElement.style.display = "none";
+          tapToPlaceElement.style.display = "block"; // Show the game instructions or elements
+        }, 1000); // Wait for 1 second for "1" to be displayed
+      }, 1000); // Wait for 1 second for "2" to be displayed
+    }, 2000); // Wait for 1 second for "3" to be displayed
+  }
+
+  // Trigger the countdown animation
+  startCountdown();
+});
+
+// Construct our ThreeJS renderer and scene as usual
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
+const scene = new THREE.Scene();
+// document.body.appendChild(renderer.domElement);
+
+// Create a Zappar camera that we'll use instead of a ThreeJS camera
+const camera = new ZapparThree.Camera();
+
+// In order to use camera and motion data, we need to ask the users for permission
+// The Zappar library comes with some UI to help with that, so let's use it
+ZapparThree.permissionRequestUI().then((granted) => {
+  // If the user granted us the permissions we need then we can start the camera
+  // Otherwise let's them know that it's necessary with Zappar's permission denied UI
+  if (granted) camera.start();
+  else ZapparThree.permissionDeniedUI();
+});
+
+ZapparThree.glContextSet(renderer.getContext());
+
+scene.background = camera.backgroundTexture;
+
+// Create an InstantWorldTracker and wrap it in an InstantWorldAnchorGroup for us
+// to put our ThreeJS content into
+const instantTracker = new ZapparThree.InstantWorldTracker();
+const instantTrackerGroup = new ZapparThree.InstantWorldAnchorGroup(
+  camera,
+  instantTracker
+);
+
+// Add our instant tracker group into the ThreeJS scene
+scene.add(instantTrackerGroup);
+
+//camera.position.z = -40;
+// THREE-JS CODE STARTS HERE
+
+const gltfLoader = new GLTFLoader(manager);
+
+let mixer: THREE.AnimationMixer;
+let modelReady = false;
+
+//------------------MODEL LOADING STARTED---------------------
+// loading models
+// gltfLoader.load(
+//   frame,
+//   (gltf) => {
+//     console.log(gltf, "frame");
+//     gltf.scene.scale.set(0.02, 0.02, 0.02);
+//     // gltf.scene.position.y = 2;
+//     gltf.scene.position.z = -9;
+//     gltf.scene.position.x = 2;
+//     gltf.scene.rotation.y -= Math.PI / 2;
+
+//     // Now the model has been loaded, we can add it to our instant_tracker_group
+//     instantTrackerGroup.add(gltf.scene);
+//   },
+//   (xhr) => {
+//     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+//   },
+//   (err) => {
+//     console.log("An error ocurred loading the GLTF model", err);
+//   }
+// );
+
+// gltfLoader.load(
+//   pokeball,
+//   (gltf) => {
+//     console.log(gltf, "pokeball");
+//     gltf.scene.scale.set(0.2, 0.2, 0.2);
+//     gltf.scene.position.y = 1.5;
+//     gltf.scene.position.z = -5;
+//     gltf.scene.position.x = -2;
+//     //gltf.scene.rotation.y -= Math.PI / 2;
+//     gltf.scene.name = "Pokeball";
+
+//     // Now the model has been loaded, we can add it to our instant_tracker_group
+//     instantTrackerGroup.add(gltf.scene);
+//   },
+//   (xhr) => {
+//     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+//   },
+//   (err) => {
+//     console.log("An error ocurred loading the GLTF model", err);
+//   }
+// );
+
+// gltfLoader.load(
+//   model,
+//   (gltf) => {
+//     console.log(gltf, "pickachuu");
+//     gltf.scene.scale.set(3, 3, 3);
+//     gltf.scene.position.y = 0;
+//     gltf.scene.position.z = -4;
+//     gltf.scene.position.x = 2;
+//     const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+//     mixer = new THREE.AnimationMixer(gltf.scene);
+
+//     // Now the model has been loaded, we can add it to our instant_tracker_group
+
+//     if (gltf.animations.length > 0) {
+//       const animationsPanel = document.getElementById(
+//         "animationsPanel"
+//       ) as HTMLDivElement;
+//       const ul = document.createElement("UL") as HTMLUListElement;
+//       const ulElem = animationsPanel.appendChild(ul);
+
+//       gltf.animations.forEach((a: THREE.AnimationClip, i) => {
+//         console.log(a, "animation_here");
+//         const li = document.createElement("UL") as HTMLLIElement;
+//         const liElem = ulElem.appendChild(li);
+
+//         const checkBox = document.createElement("INPUT") as HTMLInputElement;
+//         checkBox.id = "checkbox_" + i;
+//         checkBox.type = "checkbox";
+//         checkBox.addEventListener("change", (e: Event) => {
+//           if ((e.target as HTMLInputElement).checked) {
+//             mixer.clipAction((gltf as any).animations[i]).play();
+//           } else {
+//             mixer.clipAction((gltf as any).animations[i]).stop();
+//           }
+//         });
+//         liElem.appendChild(checkBox);
+
+//         const label = document.createElement("LABEL") as HTMLLabelElement;
+//         label.htmlFor = "checkbox_" + i;
+//         label.innerHTML = a.name;
+//         liElem.appendChild(label);
+//       });
+
+//       if (gltf.animations.length > 1) {
+//         const btnPlayAll = document.getElementById(
+//           "btnPlayAll"
+//         ) as HTMLButtonElement;
+//         btnPlayAll.addEventListener("click", (e: Event) => {
+//           mixer.stopAllAction();
+//           gltf.animations.forEach((a: THREE.AnimationClip) => {
+//             mixer.clipAction(a).play();
+//           });
+//         });
+
+//         btnPlayAll.style.display = "block";
+//       }
+//     } else {
+//       const animationsPanel = document.getElementById(
+//         "animationsPanel"
+//       ) as HTMLDivElement;
+//       animationsPanel.innerHTML = "No animations found in model";
+//     }
+
+//     instantTrackerGroup.add(gltf.scene);
+
+//     modelReady = true;
+//   },
+//   (xhr) => {
+//     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+//   },
+//   (err) => {
+//     console.log("An error ocurred loading the GLTF model", err);
+//   }
+// );
+//------------------MODEL LOADING FINISHED---------------------
+
+// const planeWidth = 1.5; // The width of the plane in your scene
+// const planeHeight = (2560 / 1920) * planeWidth; // Calculate the height to maintain the image aspect ratio
+
+// const planeGeometry = new THREE.PlaneGeometry(planeWidth, planeHeight, 1, 1);
+// // Load the pre-saved image texture
+// const textureLoader = new THREE.TextureLoader();
+// let newTexture = textureLoader.load(pokemon1);
+// console.log(newTexture);
+// const planeMaterial = new THREE.MeshBasicMaterial({ map: newTexture });
+// const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+// planeMaterial.visible = false;
+// planeMesh.receiveShadow = true;
+// planeMesh.position.set(0, 2, -5);
+// // Add the plane to the scene
+// instantTrackerGroup.add(planeMesh);
+
+//creating grid
+
+const planeMesh2 = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, visible: false })
+);
+
+planeMesh2.rotateX(-Math.PI / 2);
+planeMesh2.name = "ground";
+//planeMesh2.position.y = -3;
+
+instantTrackerGroup.add(planeMesh2);
+
+const grid = new THREE.GridHelper(20, 20);
+//grid.position.y = -3;
+instantTrackerGroup.add(grid);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const countdownElement = document.getElementById("countdown");
+  const tapToPlaceElement = document.getElementById("tap-to-place");
+
+  // Function to start the game after the countdown
+  function startGame() {
+    countdownElement.style.display = "none"; // Hide the countdown
+    tapToPlaceElement.style.display = "block"; // Show the game instructions or elements
+    // Add your game initialization logic here
+  }
+
+  // Listen for animation end event and start the game when the countdown finishes
+  countdownElement.addEventListener("animationend", startGame);
+});
+
+//highlight square
+
+// const hightSquare = new THREE.Mesh(
+//   new THREE.PlaneGeometry(1, 1),
+//   new THREE.MeshBasicMaterial({ side: THREE.DoubleSide })
+// );
+
+// hightSquare.rotateX(-Math.PI / 2);
+// hightSquare.position.set(0.5, 0, 0.5);
+
+// instantTrackerGroup.add(hightSquare);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera._updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Let's add some lighting, first a directional light above the model pointing down
+const directionalLight = new THREE.DirectionalLight("white", 1);
+directionalLight.position.set(0, 5, 0);
+directionalLight.lookAt(0, 0, 0);
+instantTrackerGroup.add(directionalLight);
+
+// And then a little ambient light to brighten the model up a bit
+const ambientLight = new THREE.AmbientLight("white", 0.4);
+instantTrackerGroup.add(ambientLight);
+
+camera.position.set(0, 10, 2);
+
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// const clock = new THREE.Clock();
+// When the experience loads we'll let the user choose a place in their room for
+// the content to appear using setAnchorPoseFromCameraOffset (see below)
+// The user can confirm the location by tapping on the screen
+let hasPlaced = false;
+const placeButton =
+  document.getElementById("tap-to-place") || document.createElement("div");
+placeButton.addEventListener("click", () => {
+  hasPlaced = true;
+  placeButton.remove();
+});
+
+console.log(instantTrackerGroup);
+
+const clock = new THREE.Clock();
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+// Use a function to render our scene as usual
+function render(): void {
+  if (!hasPlaced) {
+    // If the user hasn't chosen a place in their room yet, update the instant tracker
+    // to be directly in front of the user
+    instantTrackerGroup.setAnchorPoseFromCameraOffset(0, 0, -5);
+  }
+
+  // The Zappar camera must have updateFrame called every frame
+
+  camera.updateFrame(renderer);
+  stats.update();
+  // Draw the ThreeJS scene in the usual way, but using the Zappar camera
+  renderer.render(scene, camera);
+  // controls.update(0.01);
+
+  // camera.position.y += 1;
+  // console.log(camera.position);
+  // Call render() again next frame
+  if (modelReady) mixer.update(clock.getDelta());
+  requestAnimationFrame(render);
+}
+
+// Start things off
+render();
